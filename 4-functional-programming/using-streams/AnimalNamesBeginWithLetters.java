1679a855d7b38917ddd8356faea4ab8f9c9e8f2c
simon-towler <simon.towler@icloud.com>
2020-08-11 17:14:17 +0100

    Added cases for all types of matcher, and an infinite stream

--- 4-functional-programming/using-streams/AnimalNamesBeginWithLetters.java
+++ 4-functional-programming/using-streams/AnimalNamesBeginWithLetters.java
@@ -8,9 +8,13 @@ class AnimalNamesBeginWithLetters {
    public static void main(String... args) {
 
      List<String> animals = Arrays.asList("monkey", "2", "chimp");
-     Stream<String> infinite = Stream.generate( () -> "chimp" );
-     Predicate<String> pred = x -> Character.isLetter(x.charAt(0));
-     System.out.println(animals.stream().anyMatch(pred)); // true
+     Stream<String> infinite = Stream.generate( () -> "chimp" ); // generates an infinite stream of "chimp"
+     Predicate<String> pred = x -> Character.isLetter(x.charAt(0)); // element starts with a letter
+     // we can reuse the predicate, but we need a new stream each time
+     System.out.println(animals.stream().anyMatch(pred)); // true, because at least one does
+     System.out.println(animals.stream().allMatch(pred)); // false, because at least one doesn't
+     System.out.println(animals.stream().noneMatch(pred)); // false, because at least one does
+     System.out.println(infinite.anyMatch(pred)); // true, because at least one element does
    }
 
 }
