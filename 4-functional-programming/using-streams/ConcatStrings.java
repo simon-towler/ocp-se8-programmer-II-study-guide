ae6fe6bb7f1821a3a13ec76634ae0db7fd46ed07
simon-towler <simon.towler@icloud.com>
2020-08-13 12:06:33 +0100

    Refactor to use a method ref instead of lambda

--- 4-functional-programming/using-streams/ConcatStrings.java
+++ 4-functional-programming/using-streams/ConcatStrings.java
@@ -3,7 +3,7 @@ import java.util.stream.Stream;
 class ConcatStrings {
    public static void main(String... args) {
      Stream<String> stream = Stream.of("w", "o", "l", "f");
-     String word = stream.reduce("", (s,c) -> s + c);
+     String word = stream.reduce("", String::concat);
      System.out.println(word); // wolf
    }
 
