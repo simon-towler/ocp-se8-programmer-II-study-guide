51e1b44f28becb7c16b6737251f7a4781938a802
simon-towler <simon.towler@icloud.com>
2020-08-15 23:42:05 +0100

    refactor to use ready-made collector from Collectors interface

--- 4-functional-programming/using-streams/ConcatStrings.java
+++ 4-functional-programming/using-streams/ConcatStrings.java
@@ -1,12 +1,13 @@
 import java.util.stream.Stream;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 class ConcatStrings {
    public static void main(String... args) {
      Stream<String> stream = Stream.of("w", "o", "l", "f");
 
-     // different logic for acculator and combiner this time
-     TreeSet<String> set = stream.collect(TreeSet::new, TreeSet::add, TreeSet::addAll); // supplier, accumulator, combiner
+     // leveraging Collectors interface
+     TreeSet<String> set = stream.collect(Collectors.toCollection(TreeSet::new));
      System.out.println(set); // [f, l, o, w]
    }
 
