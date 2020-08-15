0be6939bd8b3361e4864c4fa88aa4d50df7467f2
simon-towler <simon.towler@icloud.com>
2020-08-15 23:32:13 +0100

    refactored to use different logic for accumulator and combiner

--- 4-functional-programming/using-streams/ConcatStrings.java
+++ 4-functional-programming/using-streams/ConcatStrings.java
@@ -1,13 +1,13 @@
 import java.util.stream.Stream;
+import java.util.TreeSet;
 
 class ConcatStrings {
    public static void main(String... args) {
      Stream<String> stream = Stream.of("w", "o", "l", "f");
 
-     // this works in parallel only if we don't care about the order of the letters. It could return them in almost any order.
-     // supplier, accumulator, combiner
-     StringBuilder word = stream.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
-     System.out.println(word); // wolf
+     // different logic for acculator and combiner this time
+     TreeSet<String> set = stream.collect(TreeSet::new, TreeSet::add, TreeSet::addAll); // supplier, accumulator, combiner
+     System.out.println(set); // [f, l, o, w]
    }
 
 }
