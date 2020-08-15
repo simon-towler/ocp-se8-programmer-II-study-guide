3b1f1968e784736dc7105c6cfafbdec5288113a7
simon-towler <simon.towler@icloud.com>
2020-08-15 23:26:39 +0100

    added some comments

--- 4-functional-programming/using-streams/ConcatStrings.java
+++ 4-functional-programming/using-streams/ConcatStrings.java
@@ -4,6 +4,8 @@ class ConcatStrings {
    public static void main(String... args) {
      Stream<String> stream = Stream.of("w", "o", "l", "f");
 
+     // this works in parallel only if we don't care about the order of the letters. It could return them in almost any order.
+     // supplier, accumulator, combiner
      StringBuilder word = stream.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
      System.out.println(word); // wolf
    }
