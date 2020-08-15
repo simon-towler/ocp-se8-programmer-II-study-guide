cae2795926446e0da39e7032aa0faf3c127a0c98
simon-towler <simon.towler@icloud.com>
2020-08-15 23:46:55 +0100

    refactor to simpler code that doesn't determine order

--- 4-functional-programming/using-streams/ConcatStrings.java
+++ 4-functional-programming/using-streams/ConcatStrings.java
@@ -1,14 +1,14 @@
 import java.util.stream.Stream;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
+import java.util.Set;
 
 class ConcatStrings {
    public static void main(String... args) {
      Stream<String> stream = Stream.of("w", "o", "l", "f");
 
-     // leveraging Collectors interface
-     TreeSet<String> set = stream.collect(Collectors.toCollection(TreeSet::new));
-     System.out.println(set); // [f, l, o, w]
+     // leveraging Collectors interface, without determining order
+     Set<String> set = stream.collect(Collectors.toSet());
+     System.out.println(set); // [f, w, l, o]
    }
 
 }
