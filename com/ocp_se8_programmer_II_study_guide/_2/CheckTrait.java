64e20d951aaab35b161ec07d4c639df41157f5af
simon-towler <simon.towler@icloud.com>
2023-04-23 23:27:25 +0100

    Use Predicate instead of custom fi CheckTrait

--- com/ocp_se8_programmer_II_study_guide/_2/CheckTrait.java
+++
@@ -1,5 +0,0 @@
-package com.ocp_se8_programmer_II_study_guide._2;
-@FunctionalInterface
-public interface CheckTrait {
-    public boolean test(Animal a);
-}
--- com/ocp_se8_programmer_II_study_guide/_2/FindMatchingAnimals.java
+++ com/ocp_se8_programmer_II_study_guide/_2/FindMatchingAnimals.java
@@ -1,12 +1,14 @@
 package com.ocp_se8_programmer_II_study_guide._2;
+
+import java.util.function.Predicate;
 public class FindMatchingAnimals {
-    private static void print(Animal animal, CheckTrait trait) {
+    private static void print(Animal animal, Predicate<Animal> trait) {
         if(trait.test(animal)) System.out.println(animal);
     }
 
     public static void main(String[] args) {
-        print(new Animal("fish", false, true), a -> a.canHop());
-        print(new Animal("kangaroo", true, false), a -> a.canHop());
+        print(new Animal("fish", false, true), Animal::canHop);
+        print(new Animal("kangaroo", true, false), Animal::canHop);
     }
 
 }
